5e0f65b096feaaf93e5647be616bd2fc562e4aaf
nliyanaam <nliyanaam@smu.edu.sg>
2020-10-13 14:18:46 +0800

    update

---
+++ chapter16/16.7.hs
@@ -0,0 +1,5 @@
+-- Add **fmap**, **parentheses**, and **function composition** to each expression as needed
+
+-- a = fmap (+1) $ read "[1]" :: [Int] 
+--b = fmap (++ "lol")  ("Hello", "Hi,") wrong !
+--c = fmap (*2) (\x -> x - 2)
