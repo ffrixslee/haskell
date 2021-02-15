081a3edbebd30853122a9ed9eb7c994833b58464
Liyana <nliyanaam@smu.edu.sg>
2021-02-15 18:24:21 +0800

    haskellbook: up to 24.3.hs

--- chapter24/24.3.hs
+++ chapter24/24.3.hs
@@ -1,31 +1,34 @@
 module LearnParsers where
 
-import Text.Trifecta
-import Text.Parser.Combinators
-import Control.Applicative ((<|>))
+import Text.Trifecta hiding (eof)
+import Text.Parser.Combinators hiding (unexpected)
 
 stop :: Parser a
 stop = unexpected "stop"
+-- unexpected: a means of throwing errors
 
+-- read a single character '1'
 one = char '1'
+
+-- read a single character '1', then die
 one' = one >> stop
 
+-- read two characters, '1' and '2'
 oneTwo = char '1' >> char '2'
+
+-- read two characters, '1' and '2', then die
 oneTwo' = oneTwo >> stop
 
-testParse :: Parser Char -> IO ()
+testParse :: Parser Char -> IO()
 testParse p =
     print $ parseString p mempty "123"
 
-testEOF :: Parser () -> IO ()
-testEOF p =
-    print $ parseString p mempty "123"
-
-pNL s = 
+pNL s =
     putStrLn ('\n' : s)
 
 main = do
     pNL "stop:"
+    testParse stop
 
     pNL "one:"
     testParse one
@@ -35,39 +38,12 @@ main = do
 
     pNL "oneTwo:"
     testParse oneTwo
-    
+
     pNL "oneTwo':"
     testParse oneTwo'
 
 -- Exercises: Parsing practice
-
 -- 1.
-failOne = one >> eof
-failOneTwo = oneTwo >> eof
+one'' = one >> eof
 
-test1 = do
-    pNL "failOne succeeds parsing '1':"
-    print $ parseString failOne mempty "1"
-
-    pNL "failOneTwo fails parsing '12':"
-    print $ parseString failOneTwo mempty "12"
-    
-
---oneTwoThree = (string "123" <|> string "12" <|> string "1") >> eof
--- <|> is a "choice"
-
-oneTwoThree = string "1'" >> string "2" >> string "3"
-p123 = oneTwoThree >> eof
-
-test2 = do
-    pNL "multString succeeds parsing '1':"
-    print $ parseString oneTwoThree mempty "1"
-
-    pNL "multString succeeds parsing '12':"
-    print $ parseString oneTwoThree mempty "12"
-
-    pNL "multString succeeds parsing '123':"
-    print $ parseString oneTwoThree mempty "123"
-
-
-
+oneTwo'' = oneTwo >> eof
