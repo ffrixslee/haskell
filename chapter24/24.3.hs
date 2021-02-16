b944b72e8a8d8c5387f5a97e0b0181b608c3e266
Liyana <nliyanaam@smu.edu.sg>
2021-02-16 15:01:52 +0800

    haskellbook: up to 24.5.hs

--- chapter24/24.3.hs
+++ chapter24/24.3.hs
@@ -1,7 +1,6 @@
 module LearnParsers where
 
-import Text.Trifecta hiding (eof)
-import Text.Parser.Combinators hiding (unexpected)
+import Text.Trifecta 
 
 stop :: Parser a
 stop = unexpected "stop"
@@ -22,6 +21,7 @@ oneTwo' = oneTwo >> stop
 testParse :: Parser Char -> IO()
 testParse p =
     print $ parseString p mempty "123"
+-- the p argument is a character parser
 
 pNL s =
     putStrLn ('\n' : s)
@@ -42,8 +42,45 @@ main = do
     pNL "oneTwo':"
     testParse oneTwo'
 
+
+
 -- Exercises: Parsing practice
 -- 1.
-one'' = one >> eof
+oneEof = one >> eof
+oneTwoEof = oneTwo >> eof
 
-oneTwo'' = oneTwo >> eof
+testEof = do 
+    pNL "oneEof succeeds in parsing '1':"
+    print $ parseString oneEof mempty "1"
+
+    pNL "oneTwoEof succeeds in parsing '12':"
+    print $ parseString oneTwoEof mempty "12"
+
+    pNL "oneEof fails in parsing '12':"
+    print $ parseString oneEof mempty "12"
+
+-- 2.
+testString :: Parser String -> IO()
+testString p = print $ parseString p mempty "123"
+
+p123 :: String -> Parser String
+p123 n = string n >> stop
+
+testp123 = do
+    testString $ p123 "1"
+    testString $ p123 "12"
+    testString $ p123 "123"
+    --testString $ p123 "12345"
+
+-- 3.
+testChar :: Parser Char -> IO()
+testChar p = print $ parseString p mempty "123"
+
+c123 :: Char -> Parser Char
+c123 n = char n  >> stop
+
+testc123 = do 
+    testChar $ c123 '1'
+    testChar $ c123 '2'
+    testChar $ c123 '3'
+    --testChar $ c123 '4'
